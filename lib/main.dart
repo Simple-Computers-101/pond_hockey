6f2c9ce5a8da6459c34c97bfa6c7a504f3f97e1a
Reprevise <theslimeslayer04@gmail.com>
2020-02-18 10:36:54 -0500

    Simple code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:splashscreen/splashscreen.dart';
-    
-    void main () {
-      runApp(new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: new MyApp(),
-      ));
-    }
 
-    class MyApp extends StatefulWidget {
-      @override
-      _MyAppState createState() => _MyAppState();
-    }
-    
-    class _MyAppState extends State<MyApp> {
-      @override
-      Widget build(BuildContext context) {
-        return SplashScreen(
-          seconds: 5,
-          backgroundColor: Colors.black,
-          image: Image.asset('assets/img/logo.png'),
-          loadingText: Text('Loading Pond Hockey Scoring App', style: TextStyle(fontSize: 15, color: Colors.white)),
-          loaderColor: Colors.white,
-          photoSize: 150,
-          navigateAfterSeconds: MainScreen(),
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pond Hockey',
+      home: MyApp(),
+    ),
+  );
+}
 
-        );
-      }
-    }
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 5,
+      backgroundColor: Colors.black,
+      image: Image.asset('assets/img/logo.png'),
+      loadingText: Text(
+        'Loading Pond Hockey Scoring App',
+        style: TextStyle(fontSize: 15, color: Colors.white),
+      ),
+      loaderColor: Colors.white,
+      photoSize: 150,
+      navigateAfterSeconds: MainScreen(),
+    );
+  }
+}
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(
-          title: Text('Pond Hockey Scoring'),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-        ),
-        body: new Stack(
-          children: <Widget>[
-            new Container(
-              decoration: new BoxDecoration(
-                image: new DecorationImage(
-                  image: new AssetImage("assets/img/largebg.jpg"),
-                  fit: BoxFit.cover,),
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      extendBody: true,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/img/largebg.jpg"),
+                fit: BoxFit.cover,
               ),
             ),
-
-            Center (
+          ),
+          SafeArea(
+            child: Center(
               child: Column(
                 children: <Widget>[
-                  Container(
-                    child:
-                    Image.asset('assets/img/pondhockeybrand.png'),
-                    padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: FlatButton(
-                      onPressed: (){},
-                      shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(20.0),
-                          side: BorderSide(color: Colors.black)
-                      ),
-                      child: Text(
-                        'View Results',
-                        style: TextStyle(
-                          fontSize: 30.00,
-                        ),),
-                      color: Colors.lightBlue,
-                      padding: EdgeInsets.all(8.0),
+                  Image.asset('assets/img/pondhockeybrand.png'),
+                  const SizedBox(height: 50),
+                  FlatButton(
+                    onPressed: () {},
+                    color: Colors.lightBlue,
+                    padding: EdgeInsets.all(8.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      side: BorderSide(color: Colors.black),
                     ),
-
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    child: FlatButton(
-                      onPressed: (){},
-                      shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(20.0),
-                          side: BorderSide(color: Colors.black)
+                    child: Text(
+                      'View Results',
+                      style: TextStyle(
+                        fontSize: 30,
                       ),
-                      child: Text(
-                        'Score Games',
-                        style: TextStyle(
-                          fontSize: 30.00,
-                        ),),
-                      color: Colors.lightBlue,
-                      padding: EdgeInsets.all(8.0),
                     ),
-
                   ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    child: FlatButton(
-                      onPressed: (){},
-                      shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(20.0),
-                          side: BorderSide(color: Colors.black)
-                      ),
-                      child: Text(
-                        'Create Tournament',
-                        style: TextStyle(
-                          fontSize: 30.00,
-                        ),),
-                      color: Colors.lightBlue,
-                      padding: EdgeInsets.all(8.0),
+                  const SizedBox(height: 30),
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(8.0),
+                    color: Colors.lightBlue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      side: BorderSide(color: Colors.black),
                     ),
-
+                    child: Text(
+                      'Score Games',
+                      style: TextStyle(
+                        fontSize: 30.00,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  FlatButton(
+                    onPressed: () {},
+                    color: Colors.lightBlue,
+                    padding: EdgeInsets.all(8.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      side: BorderSide(color: Colors.black),
+                    ),
+                    child: Text(
+                      'Create Tournament',
+                      style: TextStyle(
+                        fontSize: 30.00,
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-
-
-          ],
-        )
+          ),
+        ],
+      ),
     );
   }
 }
