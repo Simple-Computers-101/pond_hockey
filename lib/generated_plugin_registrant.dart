d567e1ea79b2fc396c23b28d56638562d235655b
haidar786 <hussnaintarar@gmail.com>
2020-03-08 17:59:33 +0500

    http and flutter_secure storage removed

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,7 +9,6 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
-import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -18,6 +17,5 @@ void registerPlugins(PluginRegistry registry) {
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
-  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   registry.registerMessageHandler();
 }
--- lib/services/databases/user_repository.dart
+++ lib/services/databases/user_repository.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pond_hockey/bloc/auth/auth_providers.dart';
 
@@ -65,23 +64,18 @@ class UserRepository {
   Future<void> deleteToken() async {
     /// delete from keystore/keychain
     await Future.delayed(Duration(seconds: 1));
-    final flutterSecureStorage = FlutterSecureStorage();
-    await flutterSecureStorage.delete(key: 'token');
     return;
   }
 
   Future<void> persistToken(String token) async {
     /// write to keystore/keychain
     await Future.delayed(Duration(seconds: 1));
-    final flutterSecureStorage = FlutterSecureStorage();
-    await flutterSecureStorage.write(key: 'token', value: token);
     return;
   }
 
   Future<bool> hasToken() async {
     /// read from keystore/keychain
     await Future.delayed(Duration(seconds: 1));
-    var token = await FlutterSecureStorage().read(key: 'token');
-    return token == null ? false : true;
+    return null;
   }
 }
